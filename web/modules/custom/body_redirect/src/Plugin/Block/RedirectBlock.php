33bc354f563fb2f9f1a1f3b5315f3aff420bb2c4
Rahul <sarkar.rahul0790@gmail.com>
2022-09-28 15:12:57 +0530

    Block

---
+++ web/modules/custom/body_redirect/src/Plugin/Block/RedirectBlock.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\body_redirect\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a Block with redirect image url.
+ * 
+ * @Block(
+ *   id = "redirect_image_block",
+ *   admin_label = @Translation("Redirect Image Block")
+ * )
+ */
+Class RedirectBlock extends BlockBase {
+
+/**
+ * {@inheritdoc}
+ */
+Public function build() {
+    
+ }
+
+}
+
