8cae5f9b6d0ba1085c8f8a7b3f0cb5f93d9d46b1
Rahul <sarkar.rahul0790@gmail.com>
2022-10-14 17:03:13 +0530

    new features.

--- web/modules/custom/block_example/src/Plugin/Block/ExampleBlock.php
+++ web/modules/custom/block_example/src/Plugin/Block/ExampleBlock.php
@@ -11,7 +11,7 @@ use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides a block with text and logo url
+ * Provides a block with text and logo url new.
  * 
  * @Block(
  *   id = "simple_example_block",
@@ -25,12 +25,13 @@ class ExampleBlock extends BlockBase {
  */    
 
 public function build () {
+
     return [
         '#type' => 'markup',
         '#markup' => '<div> 
-                      <p><a href="/web/"><img alt="My Logo" data-entity-type="file" data-entity-uuid="d662e458-070a-4c5d-8d34-5d62768984f8" src="/web/themes/header_image.jpg" width="95" /></a></p>
+                      <p><a href="/web/"><img alt="My Logo"  src="/web/themes/header_image.jpg" width="95" /></a></p>
                       <p>Welcome to my site.</p>
                       </div>',
            ];
-   }
-}
+  }
+ }
